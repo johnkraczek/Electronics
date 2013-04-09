bd503f5cdabb47c28e4903337bdb1fe11bc222ba
John Kraczek <John.Kraczek@gmail.com>
2013-04-08 19:28:57 -0600

    Working Mosfet Controller
    i finished the mosfet controller for my dad, it has 2 parts
    1. The arduino software which accepts commands and adjusts one of 6
    mosfets.
    2. The GUI is written in Auto it with help of KODA gui generator.
    This generates the commands and sends them to the arduino via Serial
    communication.

--- Electronics/sketch_apr04a/sketch_apr04a.ino
+++ Electronics/sketch_apr04a/sketch_apr04a.ino
@@ -1,26 +1,68 @@
-#define fadePin 13
-char inData[4]; // Allocate some space for the string
-char inChar=-1; // Where to store the character read
-byte index = 0; // Index into array; where to store the character
 
+int ledPin = 13;
+int aChamber = 2;
+int bChamber = 3;
+int cChamber = 4;
+int dChamber = 5;
+int eChamber = 6;
+int fChamber = 7;
 
-void setup(){
-  pinMode(fadePin, OUTPUT);  
-  
-  Serial.begin(9600);
+char chamber = 0;
+int value = 0;
+
+void setup() {
+Serial.begin(9600);
+
+pinMode(aChamber, OUTPUT);
+pinMode(bChamber, OUTPUT);
+pinMode(cChamber, OUTPUT);
+pinMode(dChamber, OUTPUT);
+pinMode(eChamber, OUTPUT);
+pinMode(fChamber, OUTPUT);
+
+digitalWrite(ledPin, LOW);
 }
-void loop(){
-    while (Serial.available() > 0) // Don't read unless
-                                   // there you know there is data
-    {
-        if(index < 3) // One less than the size of the array
-        {
-            inChar = Serial.read(); // Read a character
-            if(inChar == '\0'){continue}
-            inData[index] = inChar; // Store it
-            index++; // Increment where to write next
-            inData[index] = '\0'; // Null terminate the string
-        }
-    }
 
+void loop(){
+if(Serial.available() > 0) {
+
+chamber = Serial.read();
+value = Serial.parseFloat();
+
+//This helps us to know what values are going in and comming out. 
+//Serial.print(chamber);
+//Serial.println(value);
+
+
+if( chamber == 'A') {
+analogWrite(aChamber , value);
+Serial.print("Im A Im At ");
+Serial.println(value);
+}
+if (chamber == 'B'){
+analogWrite(bChamber , value);
+Serial.print("Im B Im At ");
+Serial.println(value);
+}
+if (chamber == 'C'){
+analogWrite(cChamber , value);
+Serial.print("Im C Im At ");
+Serial.println(value);
+}
+if (chamber == 'D'){
+analogWrite(dChamber , value);
+Serial.print("Im D Im At ");
+Serial.println(value);
+}
+if (chamber == 'E'){
+analogWrite(eChamber , value);
+Serial.print("Im E Im At ");
+Serial.println(value);
+}
+if (chamber == 'F'){
+analogWrite(fChamber , value);
+Serial.print("Im F Im At ");
+Serial.println(value);
+}
+}
 }
